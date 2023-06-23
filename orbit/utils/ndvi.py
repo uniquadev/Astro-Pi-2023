4694cfdd74c027607e341e18bf4a49b0a3a13a48
Diego Romeo <diego.romeo025@gmail.com>
2023-06-23 17:56:45 +0200

    updated NDVI calculations

--- orbit/utils/ndvi.py
+++ orbit/utils/ndvi.py
@@ -5,34 +5,6 @@ PYTHON MODULE FOR IMAGE PROCESSING AND NDVI CALCULATIONS
 import cv2
 import numpy as np
 
-def contrast_stretch(im):
-    in_min = np.percentile(im, 5)
-    in_max = np.percentile(im, 95)
-
-    out_min = 0.0
-    out_max = 255.0
-
-    out = im - in_min
-    out *= ((out_min - out_max) / (in_min - in_max))
-    out += in_min
-
-    return out
-
-def contrast_stretch2(im):
-    in_min = np.percentile(im, 5)
-    in_max = np.percentile(im, 95)
-
-    out_min = 0
-    out_max = 1
-
-    out = im - in_min
-    out *= ((out_min - out_max) / (in_min - in_max))
-    out += in_min
-
-    return out
-
-
-
 def ndvi(image) -> np.ndarray:
     """Calculate NDVI on the given image.
 
@@ -53,7 +25,7 @@ def mean_ndvi(image, remove_negatives=False) -> float:
     Return a float representing the mean NDVI value of the image.
     """
 
-    ndvi_array = contrast_stretch2(ndvi(image))
+    ndvi_array = ndvi(image)
     
     if remove_negatives:
         ndvi_array = [ndvi_array[i][j] for i in range(ndvi_array.shape[0]) for j in range(ndvi_array.shape[1]) if ndvi_array[i,j] >= 0]
